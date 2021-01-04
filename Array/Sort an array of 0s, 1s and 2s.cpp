ffca38fbc93cb460b73408fae9d5af812cb4224e
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-01-04 11:04:01 +0530

    Create Sort an array of 0s, 1s and 2s.cpp

---
+++ Array/Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,26 @@
+void sort012(int a[], int n)
+{
+    int lo=0,mid=0,high=n-1;
+    
+    while(mid<=high){
+        
+        switch(a[mid]){
+            
+            case 0:
+            swap(a[lo],a[mid]);
+            lo++;
+            mid++;
+            break;
+            
+            case 1:
+            mid++;
+            break;
+            
+            case 2:
+            swap(a[mid],a[high]);
+            high--;
+            break;
+            
+        }
+    }
+}
