929e177563c70ec99c8e5e5a02ba6c24cbed49a3
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-02-14 17:28:04 +0530

    Update Longest Common Substring.cpp

--- Dynamic Programming/Longest Common Substring.cpp
+++ Dynamic Programming/Longest Common Substring.cpp
@@ -1,24 +1,37 @@
-int lcs(int x, int y, string s1, string s2){
+class Solution{
+    public:
     
-    int dp[x+1][y+1];
-    
-    for(int j = 0; j < y + 1; j++){
-        dp[0][j] = 0;
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+       int dp[n+1][m+1];
+       
+       for(int j = 0; j < m + 1; j++){
+           dp[0][j] = 0;
+       }
+       
+       for(int i = 0; i < n + 1; i++){
+           dp[i][0] = 0;
+       }
+       
+       for(int i = 1; i < n + 1; i++){
+           for(int j = 1; j < m + 1; j++){
+               
+               if(S1[i-1] == S2[j-1]){
+                   dp[i][j] = 1 + dp[i-1][j-1];
+               }
+               else{
+                   dp[i][j] = 0;
+               }
+           }
+       }
+       
+       int ans = INT_MIN;
+       
+       for(int i = 0; i < n + 1; i++){
+           for(int j = 0; j < m + 1; j++){
+               ans = max(ans,dp[i][j]);
+           }
+       }
+       return ans;
     }
-    for(int i = 0; i < x + 1; i++){
-        dp[i][0] = 0;
-    }
-    
-    for(int i = 1; i < x + 1; i++){
-        for(int j = 1; j < y + 1; j++){
-            
-            if(s1[i-1] == s2[j-1]){
-                dp[i][j] = 1 + dp[i-1][j-1];
-            }
-            else{
-                dp[i][j] = 0;
-            }
-        }
-    }
-    return dp[x][y];
-}
+};
