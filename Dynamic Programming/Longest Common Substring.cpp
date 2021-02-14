553649ed9eecc2c4936260907a25bfff3e0c7865
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-02-14 17:27:18 +0530

    Update Longest Common Substring.cpp

--- Dynamic Programming/Longest Common Substring.cpp
+++ Dynamic Programming/Longest Common Substring.cpp
@@ -16,7 +16,7 @@ int lcs(int x, int y, string s1, string s2){
                 dp[i][j] = 1 + dp[i-1][j-1];
             }
             else{
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                dp[i][j] = 0;
             }
         }
     }
