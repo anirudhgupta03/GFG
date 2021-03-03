0a547822b3c4fe6de2c34a32eecdf2fb9241168c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-03-03 19:57:14 +0530

    Create Number of 1 Bits.cpp

---
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int setBits(int N) {
+        
+        int count = 0;
+        
+        while(N){
+            N = (N & (N - 1));
+            count++;
+        }
+        return count;
+    }
+};
