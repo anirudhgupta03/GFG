8a910acd82278d05fa2b75d9bbf18ea559687f9b
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-09 10:50:05 +0530

    Create Floyd Warshall.cpp

---
+++ Graph/Floyd Warshall.cpp
@@ -0,0 +1,22 @@
+class Solution{
+public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    
+	    int V = matrix.size();
+	    
+	    for(int k = 0; k < V; k++){
+	        for(int i = 0; i < V; i++){
+	            for(int j = 0; j < V; j++){
+	                
+	                if(matrix[i][k] == -1 || matrix[k][j] == -1){
+	                    continue;
+	                }
+	                
+	                if(matrix[i][k] + matrix[k][j] < matrix[i][j]){
+	                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+	                }
+	            }
+	        }
+	    }
+	}
+};
