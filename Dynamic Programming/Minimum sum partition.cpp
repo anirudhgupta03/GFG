da7e2903e76324d4a55610c594b5b545c68518c7
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-17 18:09:46 +0530

    Recursive | Top-Down | Bottom-Up

--- Dynamic Programming/Minimum sum partition.cpp
+++ Dynamic Programming/Minimum sum partition.cpp
@@ -1,3 +1,67 @@
+//Recursive Approach
+//Time Complexity - O(2^N)
+//Space Complexity - O(1)
+class Solution{
+  public:
+    int solve(int ind, int temp, int sum, int arr[], int n){
+        
+        if(ind == n){
+            return abs(sum-2*temp);
+        }
+        
+        return min(solve(ind+1,temp+arr[ind],sum,arr,n), solve(ind+1,temp,sum,arr,n));
+    }
+    int minDifference(int arr[], int n)  { 
+	    
+	int sum = 0;
+	    
+	for(int i = 0; i < n; i++){
+	     sum += arr[i];
+	}
+	    
+	int temp = 0;
+	    
+	return solve(0,temp,sum,arr,n);
+    } 
+};
+
+//Top-Down Approach
+//Time Complexity - O(N*sum)
+//Space Complexity - O(N*sum)
+class Solution{
+  public:
+    int solve(int n, int temp, int sum, int arr[], vector<vector<int>> &dp){
+        
+        if(n == 0){
+            return dp[n][temp] = abs(sum-2*temp);
+        }
+        
+        if(dp[n][temp] != -1){
+            return dp[n][temp];
+        }
+        
+        return dp[n][temp] = min(solve(n-1,temp+arr[n-1],sum,arr,dp), solve(n-1,temp,sum,arr,dp));
+    }
+    int minDifference(int arr[], int n)  { 
+	    
+	int sum = 0;
+	    
+	for(int i = 0; i < n; i++){
+	    sum += arr[i];
+	}
+	    
+	int temp = 0;
+	    
+	vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+	    
+	return solve(n,temp,sum,arr,dp);
+    } 
+};
+
+
+//Bottom-Up Approach
+//Time Complexity - O(N*sum)
+//Space Complexity - O(N*sum)
 class Solution{
 
 	public:
