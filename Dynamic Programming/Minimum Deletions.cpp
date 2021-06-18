33154c8275158004da6cefb26603d71535745f3b
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 18:54:28 +0530

    Bottom-Up

--- Dynamic Programming/Minimum Deletions.cpp
+++ Dynamic Programming/Minimum Deletions.cpp
@@ -1,43 +1,30 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	
-	int t;
-	cin >> t;
-	
-	while(t--){
-	    
-	    string s;
-	    cin >> s;
-	    
-	    string p;
-	    for(int i = s.size() - 1; i >= 0; i--){
-	        p.push_back(s[i]);
-	    }
-	    
-	    int dp[s.size() + 1][p.size() + 1];
-	    
-	    for(int j = 0; j < s.size() + 1; j++){
-	        dp[0][j] = 0;
-	    }
-	    
-	    for(int i = 0; i < p.size() + 1; i++){
-	        dp[i][0] = 0;
-	    }
-	    
-	    for(int i = 1; i < s.size() + 1; i++){
-	        for(int j = 1; j < p.size() + 1; j++){
-	            
-	            if(s[i-1] == p[j-1]){
-	                dp[i][j] = 1 + dp[i-1][j-1];
-	            }
-	            else{
-	                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
-	            }
-	        }
-	    }
-	    
-	    cout << s.size() - dp[s.size()][p.size()] << endl;
-	}
-	return 0;
-}
+//Time Complexity - O(len^2)
+//Space Complexity - O(len^2)
+class Solution{
+  public:
+    int minimumNumberOfDeletions(string s) { 
+        
+        string rev = s;
+        reverse(rev.begin(),rev.end());
+        
+        int len = s.size();
+        
+        int dp[len+1][len+1];
+        
+        for(int i = 0; i <= len; i++){
+            for(int j = 0; j <= len; j++){
+                
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(s[i-1] == rev[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return len - dp[len][len];
+    } 
+};
