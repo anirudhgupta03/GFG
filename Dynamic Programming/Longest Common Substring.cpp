23bb2ba078f44030233284c5b316dc061c5a59cf
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 17:58:14 +0530

    Bottom-Up

--- Dynamic Programming/Longest Common Substring.cpp
+++ Dynamic Programming/Longest Common Substring.cpp
@@ -1,37 +1,30 @@
+//Bottom-Up Approach
+//Time Complexity - O(M*N)
+//Space Complexity - O(M*N)
 class Solution{
     public:
     
     int longestCommonSubstr (string S1, string S2, int n, int m)
     {
-       int dp[n+1][m+1];
-       
-       for(int j = 0; j < m + 1; j++){
-           dp[0][j] = 0;
-       }
-       
-       for(int i = 0; i < n + 1; i++){
-           dp[i][0] = 0;
-       }
-       
-       for(int i = 1; i < n + 1; i++){
-           for(int j = 1; j < m + 1; j++){
-               
-               if(S1[i-1] == S2[j-1]){
-                   dp[i][j] = 1 + dp[i-1][j-1];
-               }
-               else{
-                   dp[i][j] = 0;
-               }
-           }
-       }
-       
-       int ans = INT_MIN;
-       
-       for(int i = 0; i < n + 1; i++){
-           for(int j = 0; j < m + 1; j++){
-               ans = max(ans,dp[i][j]);
-           }
-       }
-       return ans;
+        int dp[n+1][m+1];
+        int result = 0;
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= m; j++){
+                
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(S1[i-1] == S2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    result = max(result,dp[i][j]);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        return result;
     }
 };
