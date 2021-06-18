9f6ba32ab0b7a296054c552127e3ca7bf5f06d96
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 20:24:36 +0530

    Recursive | Top-Down | Bottom-Up

--- Dynamic Programming/Longest Repeating Subsequence.cpp
+++ Dynamic Programming/Longest Repeating Subsequence.cpp
@@ -1,3 +1,64 @@
+//Recursive Approach
+//Time Complexity - O(2^N)
+//Space Complexity - O(1)
+class Solution{
+	    public:
+	    int solve(int n1, int n2, string s1, string s2){
+	        
+	        if(n1 == 0 || n2 == 0){
+	            return 0;
+	        }
+	        
+	        if(s1[n1-1] == s2[n2-1] && n1 != n2){
+	            return 1 + solve(n1-1,n2-1,s1,s2);
+	        }
+	        else{
+	            return max(solve(n1-1,n2,s1,s2), solve(n1,n2-1,s1,s2));
+	        }
+	    }
+		int LongestRepeatingSubsequence(string str){
+		    
+		    int n = str.size();
+		    return solve(n,n,str,str);
+		}
+
+};
+
+//Top-Down Approach
+//Time Complexity - O(N^2)
+//Space Complexity - O(N^2)
+class Solution{
+	    public:
+	    int solve(int n1, int n2, string &s1, string &s2, vector<vector<int>> &dp){
+	        
+	        if(n1 == 0 || n2 == 0){
+	            return dp[n1][n2] = 0;
+	        }
+	        
+	        if(dp[n1][n2] != -1){
+	            return dp[n1][n2];
+	        }
+	        if(s1[n1-1] == s2[n2-1] && n1 != n2){
+	            return dp[n1][n2] = 1 + solve(n1-1,n2-1,s1,s2,dp);
+	        }
+	        else{
+	            return dp[n1][n2] = max(solve(n1-1,n2,s1,s2,dp), solve(n1,n2-1,s1,s2,dp));
+	        }
+	    }
+		int LongestRepeatingSubsequence(string str){
+		    
+		    int n = str.size();
+		    
+		    vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+		    
+		    return solve(n,n,str,str,dp);
+		}
+
+};
+
+//Bottom-Up Approach
+//Time Complexity - O(N^2)
+//Space Complexity - O(N^2)
 class Solution {
 	public:
 		int LongestRepeatingSubsequence(string str){
