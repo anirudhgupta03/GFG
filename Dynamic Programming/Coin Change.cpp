bbb4859082f40aad1a2ab074fcdca0dba1b98c7b
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 15:08:04 +0530

    Recursive | Top-Down | Bottom-Up

--- Dynamic Programming/Coin Change.cpp
+++ Dynamic Programming/Coin Change.cpp
@@ -1,3 +1,63 @@
+//Recursive Approach
+//Time Complexity - O(2^m)
+//Space Complexity - O(1)
+class Solution{
+{
+  public:
+    long long int count( int S[], int m, int n )
+    {
+       if(n == 0){
+           return 1;
+       }
+       if(m == 0){
+           return 0;
+       }
+       
+       if(S[m-1] <= n){
+           return count(S,m,n-S[m-1]) + count(S,m-1,n);
+       }
+       else{
+           return count(S,m-1,n);
+       }
+    }
+};
+
+//Top-Down Approach
+//Time Complexity - O(m*n)
+//Space Complexity - O(m*n)
+{
+  public:
+    long long int solve(int m, int n, int S[], vector<vector<long long int>> &dp){
+        
+        if(n == 0){
+            return dp[m][n] = 1;
+        }
+        if(m == 0){
+            return dp[m][n] = 0;
+        }
+        
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        
+        if(S[m-1] <= n){
+            return dp[m][n] = solve(m,n-S[m-1],S,dp) + solve(m-1,n,S,dp);
+        }
+        else{
+            return dp[m][n] = solve(m-1,n,S,dp);
+        }
+    }
+    long long int count( int S[], int m, int n )
+    {
+       vector<vector<long long int>> dp(m+1,vector<long long int>(n+1,-1));
+       
+       return solve(m,n,S,dp);
+    }
+};
+  
+//Bottom-Up Approach
+//Time Complexity - O(m*n)
+//Space Complexity - O(m*n)
 class Solution
 {
   public:
@@ -5,7 +65,7 @@ class Solution
     {
        long long dp[m+1][n+1];
        
-       for(int j = 0; j < n+1 ; j++){
+       for(int j = 1; j < n+1 ; j++){
            dp[0][j] = 0;
        }
        
