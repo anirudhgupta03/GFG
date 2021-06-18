2a87f011f82798dc75f4b57761f4e99eab548fea
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 18:28:09 +0530

    Recursive | Bottom-Up

--- Dynamic Programming/Shortest Common Supersequence.cpp
+++ Dynamic Programming/Shortest Common Supersequence.cpp
@@ -1,3 +1,30 @@
+//Recursive Approach
+class Solution
+{
+    public:
+    //Function to find length of shortest common supersequence of two strings.
+    int lcs(string X, string Y, int m, int n){
+        
+        if(m == 0 || n == 0){
+            return 0;
+        }
+        
+        if(X[m-1] == Y[n-1]){
+            return 1 + lcs(X,Y,m-1,n-1);
+        }
+        else{
+            return max(lcs(X,Y,m-1,n), lcs(X,Y,m,n-1));
+        }
+    }
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        return m + n - lcs(X,Y,m,n);   
+    }
+};
+
+//Bottom-Up Approach
+//Time Complexity - O(m*n)
+//Space Complexity - O(m*n)
 int shortestCommonSupersequence(char* X, char* Y, int m, int n) {
     
     int dp[m+1][n+1];
