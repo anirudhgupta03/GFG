6fe3a0a75f8ee69c3270a8210733531fe6f2b1f5
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 16:05:04 +0530

    Top-Down

---
+++ Dynamic Programming/Maximize The Cut Segments .cpp
@@ -0,0 +1,44 @@
+//Ref: youtube.com/watch?v=O0N4xn38Ncg
+//Top-Down Approach
+//Time Complexity - O(N)
+//Space Complexity - O(N)
+class Solution
+{
+    public:
+    //Function to find the maximum number of cuts.
+   int solve(int n, int x, int y, int z, int dp[]){
+        
+        if(n == 0){
+            return dp[n] = 0;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int a = INT_MIN,b = INT_MIN,c = INT_MIN;
+        
+        if(x <= n){
+            a = solve(n-x,x,y,z,dp) + 1;
+        }
+        if(y <= n){
+            b = solve(n-y,x,y,z,dp) + 1;
+        }
+        if(z <= n){
+            c = solve(n-z,x,y,z,dp) + 1;
+        }
+        
+        return dp[n] = max(a,max(b,c));
+    }
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        
+        int ans = solve(n,x,y,z,dp);
+        
+        if(ans < 0){
+            return 0;
+        }
+        return ans;
+    }
+};
