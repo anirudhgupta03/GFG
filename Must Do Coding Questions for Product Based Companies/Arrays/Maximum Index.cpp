9d4607a4f3069a49b551bdb1622dd23d1865c62d
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-01 14:16:34 +0530

    Create Maximum Index.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Arrays/Maximum Index.cpp
@@ -0,0 +1,29 @@
+class Solution{
+    public:
+        
+    // arr[]: input array
+    // n: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int arr[], int n) 
+    { 
+        int right[n];
+        
+        right[n-1] = arr[n-1];
+        
+        for(int i = n - 2; i >= 0; i--){
+            right[i] = max(arr[i],right[i+1]);
+        }
+        int ans = 0, j = 0;
+        
+        for(int i = 0; i < n; i++){
+            
+            while(arr[i] <= right[j] && j < n){
+                j++;
+            }
+            ans = max(ans,j-i-1);
+            if(j == n)
+            break;
+        }
+        return ans;
+    }
+};
