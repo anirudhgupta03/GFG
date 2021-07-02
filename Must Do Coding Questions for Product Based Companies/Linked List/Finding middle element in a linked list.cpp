5070ed5ce69753e9751cf46c634afb1ca5b59b85
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-02 15:55:50 +0530

    Create Finding middle element in a linked list.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Linked List/Finding middle element in a linked list.cpp
@@ -0,0 +1,24 @@
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   Node* slow = head, *fast = head;
+   
+   while(fast != NULL && fast -> next != NULL){
+       slow = slow -> next;
+       fast = fast -> next -> next;
+   }
+   
+   return slow -> data;
+}
