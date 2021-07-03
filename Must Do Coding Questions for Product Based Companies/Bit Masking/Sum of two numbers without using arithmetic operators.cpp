d0c2ecf2321a20d3bcee42c631091347d56172bd
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-03 10:03:32 +0530

    Create Sum of two numbers without using arithmetic operators.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Bit Masking/Sum of two numbers without using arithmetic operators.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    int sum(int a , int b)
+    {
+        while(b){
+            
+            int carry = a & b;
+            
+            a = a ^ b;
+            
+            b = carry << 1;
+        }
+        return a;
+    }
+};
