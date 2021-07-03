9c46cc79e052f0c6dc6a74b96e1402f181846067
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-03 09:50:07 +0530

    Create Number of 1 Bits.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Bit Masking/Number of 1 Bits.cpp
@@ -0,0 +1,14 @@
+public:
+    int setBits(int N) {
+        
+        int count = 0;
+        
+        while(N){
+            
+            N = (N & (N - 1));
+            count++;
+        }
+        
+        return count;
+    }
+};
