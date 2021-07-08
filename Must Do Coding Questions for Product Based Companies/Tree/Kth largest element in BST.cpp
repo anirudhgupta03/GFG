005934f17d4909436567e3e21a64673739f831e1
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-08 15:23:32 +0530

    Create Kth largest element in BST.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Tree/Kth largest element in BST.cpp
@@ -0,0 +1,25 @@
+// return the Kth largest element in the given BST rooted at 'root'
+class Solution
+{
+    public:
+    void solve(Node* root, int &K, int &ans){
+        
+        if(root == NULL){
+            return;
+        }
+        
+        solve(root -> right, K, ans);
+        K--;
+        if(K == 0){
+            ans = root -> data;
+            return;
+        }
+        solve(root -> left, K, ans);
+    }
+    int kthLargest(Node *root, int K)
+    {
+        int ans;
+        solve(root,K,ans);
+        return ans;
+    }
+};
