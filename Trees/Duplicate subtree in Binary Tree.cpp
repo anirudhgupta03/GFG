562b7151f43bd02c62a5ef6561bf93eabbfcd954
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-19 16:00:23 +0530

    Create Duplicate subtree in Binary Tree.cpp

---
+++ Trees/Duplicate subtree in Binary Tree.cpp
@@ -0,0 +1,27 @@
+//Ref: https://www.youtube.com/watch?v=_j7yb_nWFO8
+class Solution {
+  public:
+    /*This function returns true if the tree contains 
+    a duplicate subtree of size 2 or more else returns false*/
+    string solve(Node* root, unordered_map<string,int> &m){
+        if(root == NULL) return "$";
+        string s = "";
+        if(root -> left == NULL && root -> right == NULL){
+            s = to_string(root -> data);
+            return s;
+        }
+        s = s + to_string(root -> data);
+        s = s + solve(root -> left, m);
+        s = s + solve(root -> right, m);
+        m[s]++;
+        return s;
+    }
+    int dupSub(Node *root) {
+         unordered_map<string,int> m;
+         solve(root,m);
+         for(auto x: m){
+             if(x.second >= 2) return 1;
+         }
+         return 0;
+    }
+};
