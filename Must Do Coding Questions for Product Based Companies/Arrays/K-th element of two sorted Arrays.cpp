b56e9daccdc6f0d2d2f17d7ed6575f7b25e3474e
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-20 20:49:13 +0530

    Update K-th element of two sorted Arrays.cpp

--- Must Do Coding Questions for Product Based Companies/Arrays/K-th element of two sorted Arrays.cpp
+++ Must Do Coding Questions for Product Based Companies/Arrays/K-th element of two sorted Arrays.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution{  
   public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
@@ -38,3 +39,68 @@ class Solution{
             }
         }
     }
+
+ //Method - 2
+  class Solution{
+      public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        if(n > m) return kthElement(arr2,arr1,m,n,k);
+        
+        int l = 0, r = n;
+        
+        while(l <= r){
+            
+            int cut1 = (l + r)/2;
+            int cut2 = k - cut1;
+            
+            if(cut2 > m){
+                l = cut1 + 1;
+                continue;
+            }
+            if(cut2 < 0){
+                r = cut1 - 1;
+                continue;
+            }
+            int a1,b1;
+            
+            if(cut1 == 0){
+                a1 = INT_MIN;
+                b1 = arr1[cut1];
+            }
+            else if(cut1 == n){
+                a1 = arr1[n-1];
+                b1 = INT_MAX;
+            }
+            else{
+                a1 = arr1[cut1-1];
+                b1 = arr1[cut1];
+            }
+            
+            int a2,b2;
+            
+            if(cut2 == 0){
+                a2 = INT_MIN;
+                b2 = arr2[cut2];
+            }
+            else if(cut2 == m){
+                a2 = arr2[m-1];
+                b2 = INT_MAX;
+            }
+            else{
+                a2 = arr2[cut2-1];
+                b2 = arr2[cut2];
+            }
+            
+            if(a1 <= b2 && a2 <= b1){
+                return max(a1,a2);
+            }
+            else if(a1 > b2){
+                r = cut1 - 1;
+            }
+            else{
+                l = cut1 + 1;
+            }
+        }
+    }
+}
