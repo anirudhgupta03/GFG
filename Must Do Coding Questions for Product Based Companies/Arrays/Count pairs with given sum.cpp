b94bce752457468b551026900be22bb92ce2d652
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-21 22:33:48 +0530

    Create Count pairs with given sum.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Arrays/Count pairs with given sum.cpp
@@ -0,0 +1,20 @@
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        
+        unordered_map<int,int> umap;
+        
+        int count = 0;
+        
+        for(int i = 0; i < n; i++){
+            
+            if(umap.find(k-arr[i]) != umap.end()){
+                count += umap[k-arr[i]];
+            }
+            
+            umap[arr[i]]++;
+        }
+        
+        return count;
+    }
+};
