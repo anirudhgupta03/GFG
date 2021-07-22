5d8b144052207528a9430ad3d3252045973348a9
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-22 10:54:53 +0530

    Create Minimum swaps and K together.cpp

---
+++ Array/Minimum swaps and K together.cpp
@@ -0,0 +1,35 @@
+int minSwap(int *arr, int n, int k) {
+    
+    int good = 0, bad = 0;
+    
+    for(int i = 0; i < n; i++){
+        if(arr[i] <= k){
+            good++;
+        }
+    }
+    
+    for(int i = 0; i < good; i++){
+        if(arr[i] > k){
+            bad++;
+        }
+    }
+    
+    int l = 0, r = good-1;
+    int ans = 1e9;
+    
+    while(r < n){
+        
+        ans = min(ans,bad);
+        
+        r++;
+        if(arr[r] > k){
+            bad++;
+        }
+        if(arr[l] > k){
+            bad--;
+        }
+        l++;
+    }
+    
+    return ans;
+}
