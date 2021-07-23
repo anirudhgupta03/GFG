b05784f096fea507f3c6105d4f3acd53e7c49415
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-24 01:38:07 +0530

    Update Detect cycle in a directed graph.cpp

--- Must Do Coding Questions for Product Based Companies/Graph/Detect cycle in a directed graph.cpp
+++ Must Do Coding Questions for Product Based Companies/Graph/Detect cycle in a directed graph.cpp
@@ -1,3 +1,4 @@
+//Using DFS
 class Solution
 {
     public:
@@ -35,5 +36,51 @@ class Solution
 	    }
 	    
 	    return false;
+	}
+};
+
+//Using BFS(Topological  Sort works only in Directed Acyclic Graph)
+//Ref: https://www.youtube.com/watch?v=V6GxfKDyLBM
+class Solution
+{
+    public:
+	//Function to detect cycle in a directed graph.
+	bool isCyclic(int V, vector<int> adj[]) 
+	{
+	   	vector<int> in(V,0);
+	   	
+	   	for(int i = 0; i < V; i++){
+	   	    vector<int> v = adj[i];
+	   	    for(int j = 0; j < v.size(); j++){
+	   	        in[v[j]]++;
+	   	    }
+	   	}
+	   	
+	   	queue<int> q;
+	   	for(int i = 0; i < V; i++){
+	   	    if(in[i] == 0){
+	   	        q.push(i);
+	   	    }
+	   	}
+	   	
+	   	vector<int> res;
+	   	while(!q.empty()){
+	   	    
+	   	    int curr = q.front();
+	   	    res.push_back(curr);
+	   	    q.pop();
+	   	    
+	   	    for(int i = 0; i < adj[curr].size(); i++){
+	   	        in[adj[curr][i]]--;
+	   	        if(in[adj[curr][i]] == 0){
+	   	            q.push(adj[curr][i]);
+	   	        }
+	   	    }
+	   	}
+	   	
+	   	if(res.size() == V){
+	   	    return false;
+	   	}
+	   	return true;
 	}
 };
