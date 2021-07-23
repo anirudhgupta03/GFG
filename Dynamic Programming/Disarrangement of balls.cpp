79402ce7ef08a74b5009b79cb3ec2eecc87db8bd
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-23 12:44:58 +0530

    Create Disarrangement of balls.cpp

---
+++ Dynamic Programming/Disarrangement of balls.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    long int disarrange(int N){
+        
+        int mod = 1e9 + 7;
+        if(N <= 2){
+            return N - 1; 
+        }
+        long int dp[N+1];
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        
+        for(int i = 3; i <= N; i++){
+            dp[i] = ((i - 1)*(dp[i-1] + dp[i-2])%mod)%mod;
+        }
+        
+        return dp[N];
+    }
+};
