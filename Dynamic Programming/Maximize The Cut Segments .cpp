7e5db220f03dd5d67ff529c08a7118eeda9e8b8a
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-02 13:09:50 +0530

    Update Maximize The Cut Segments .cpp

--- Dynamic Programming/Maximize The Cut Segments .cpp
+++ Dynamic Programming/Maximize The Cut Segments .cpp
@@ -42,3 +42,48 @@ class Solution
         return ans;
     }
 };
+
+//Better
+class Solution
+{
+    public:
+    //Function to find the maximum number of cuts.
+    int solve(int n, int x, int y, int z, int dp[]){
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        int ans = INT_MIN;
+        
+        if(n >= x){
+            int t1 = solve(n - x, x, y, z, dp);
+            if(t1 != INT_MIN) ans = max(ans, t1 + 1);
+        }
+        if(n >= y){
+            int t2 = solve(n - y, x, y, z, dp);
+            if(t2 != INT_MIN) ans = max(ans, t2 + 1);
+        }
+        if(n >= z){
+            int t3 = solve(n - z, x, y, z, dp);
+            if(t3 != INT_MIN) ans = max(ans, t3 + 1);
+        }
+        
+        return dp[n] = ans;
+    }
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        int res = solve(n, x, y, z, dp);
+        if(res == INT_MIN){
+            return 0;
+        }
+        return res;
+    }
+};
