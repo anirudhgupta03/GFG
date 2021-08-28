e36413a7b9fe58f95c5b867b716ffd9129e19f2d
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-28 14:09:22 +0530

    Update Find Pair Given Difference.cpp

--- Must Do Coding Questions for Product Based Companies/Searching/Find Pair Given Difference.cpp
+++ Must Do Coding Questions for Product Based Companies/Searching/Find Pair Given Difference.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//Time Complexity - O(N)
+//Space Complexity - O(N)
 bool findPair(int arr[], int size, int n){
    
    unordered_map<int,int> umap;
@@ -12,4 +15,36 @@ bool findPair(int arr[], int size, int n){
    }
    
    return false;
+}
+
+//Method - 2
+//Time Complexity - O(NlogN)
+//Space Complexity - O(1)
+//Ref: https://www.youtube.com/watch?v=5IUyc7cPD9E
+bool findPair(int arr[], int size, int n){
+    
+    sort(arr,arr+size);
+    
+    for(int i = 0; i < size; i++){
+        
+        int ele = arr[i] + n;
+        
+        int lo = i + 1, hi = size - 1;
+        
+        while(lo <= hi){
+            
+            int mid = (lo + hi)/2;
+            
+            if(arr[mid] == ele){
+                return true;
+            }
+            else if(arr[mid] > ele){
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+    }
+    return false;
 }
