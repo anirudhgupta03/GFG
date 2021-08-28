af3c053ef4df41f42602547eb8cb9ac72a012657
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-28 14:51:06 +0530

    Update Zero Sum Subarrays.cpp

--- Must Do Coding Questions for Product Based Companies/Searching/Zero Sum Subarrays.cpp
+++ Must Do Coding Questions for Product Based Companies/Searching/Zero Sum Subarrays.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution{
     public:
     //Function to count subarrays with sum equal to 0.
@@ -16,6 +17,33 @@ class Solution{
             if(sum == 0){
                 count++;
             }
+            
+            if(umap.find(sum) != umap.end()){
+                count += umap[sum];
+            }
+            
+            umap[sum]++;
+        }
+        
+        return count;
+    }
+};
+
+//Method - 2
+class Solution{
+    public:
+    //Function to count subarrays with sum equal to 0.
+    ll findSubarray(vector<ll> arr, int n ) {
+        
+        unordered_map<int,int> umap;
+        
+        ll count = 0, sum = 0;
+        
+        umap[0] = 1;
+        
+        for(int i = 0; i < n; i++){
+            
+            sum += arr[i];
             
             if(umap.find(sum) != umap.end()){
                 count += umap[sum];
