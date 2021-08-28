3ea88e9c898bb708e0551675a901b797c9f4a236
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-28 14:32:04 +0530

    Update Stickler Thief.cpp

--- Must Do Coding Questions for Product Based Companies/Searching/Stickler Thief.cpp
+++ Must Do Coding Questions for Product Based Companies/Searching/Stickler Thief.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution
 {
     public:
@@ -23,5 +24,29 @@ class Solution
         }
         
         return max(max1,max2);
+    }
+};
+
+//Method - 2
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        if(n == 1){
+            return arr[0];
+        }
+        
+        int dp[n];
+        
+        dp[0] = arr[0];
+        dp[1] = max(arr[0], arr[1]);
+        
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i-2] + arr[i], dp[i-1]);
+        }
+        
+        return max(dp[n-1],dp[n-2]);
     }
 };
