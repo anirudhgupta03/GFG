bf80c0efb2e1a0fbe836b84140d4de77a1d5ea27
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-09-24 17:57:36 +0530

    Create Shuffle Two Strings.cpp

---
+++ Must Do Coding Questions for Product Based Companies/String/Shuffle Two Strings.cpp
@@ -0,0 +1,68 @@
+//Method - 1
+//TLE
+bool solve(string a, string b, string c, int n1, int n2, int n3){
+    
+    if(n1 == 0 && n2 == 0 && n3 == 0){
+        return true;
+    }
+    
+    if(n1 > 0 && n2 > 0 && a[n1 - 1] == c[n3 - 1] && b[n2 - 1] == c[n3 - 1]){
+        return solve(a, b, c, n1 - 1, n2, n3 - 1) || solve(a, b, c, n1, n2 - 1, n3 - 1);
+    }
+    else if(n1 > 0 && a[n1 - 1] == c[n3 - 1]){
+        return solve(a, b, c, n1 - 1, n2, n3 - 1);
+    }
+    else if(n2 > 0 && b[n2 - 1] == c[n3 - 1]){
+        return solve(a, b, c, n1, n2 - 1, n3 - 1);
+    }
+    else{
+        return false;
+    }
+}
+bool isInterleave(string a, string b, string c){
+	
+    int n1 = a.size(), n2 = b.size(), n3 = c.size();
+    
+    if(n1 + n2 != n3){
+        return false;
+    }
+    
+    return solve(a, b, c, n1, n2, n3);
+}
+
+//Method - 2
+//DP
+bool solve(string a, string b, string c, int n1, int n2, int n3, vector<vector<int>> &dp){
+    
+    if(n1 == 0 && n2 == 0 && n3 == 0){
+        return true;
+    }
+    
+    if(dp[n1][n2] != -1){
+        return dp[n1][n2];
+    }
+    
+    if(n1 > 0 && n2 > 0 && a[n1 - 1] == c[n3 - 1] && b[n2 - 1] == c[n3 - 1]){
+        return dp[n1][n2] = solve(a, b, c, n1 - 1, n2, n3 - 1, dp) || solve(a, b, c, n1, n2 - 1, n3 - 1, dp);
+    }
+    else if(n1 > 0 && a[n1 - 1] == c[n3 - 1]){
+        return dp[n1][n2] = solve(a, b, c, n1 - 1, n2, n3 - 1, dp);
+    }
+    else if(n2 > 0 && b[n2 - 1] == c[n3 - 1]){
+        return dp[n1][n2] = solve(a, b, c, n1, n2 - 1, n3 - 1, dp);
+    }
+    else{
+        return dp[n1][n2] = false;
+    }
+}
+bool isInterleave(string a, string b, string c){
+	
+    int n1 = a.size(), n2 = b.size(), n3 = c.size();
+    
+    if(n1 + n2 != n3){
+        return false;
+    }
+    
+    vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+    return solve(a, b, c, n1, n2, n3, dp);
+}
