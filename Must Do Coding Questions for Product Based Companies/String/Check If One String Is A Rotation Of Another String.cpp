67def737c56aaff569ae83dbe45f43489b3987ca
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-09-24 17:07:12 +0530

    KMP Algorithm

--- Must Do Coding Questions for Product Based Companies/String/Check If One String Is A Rotation Of Another String.cpp
+++ Must Do Coding Questions for Product Based Companies/String/Check If One String Is A Rotation Of Another String.cpp
@@ -28,3 +28,52 @@ int isCyclicRotation(string &p, string &q)
 }
 
 //Method - 2
+//Ref1: https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+//Ref2: https://www.youtube.com/watch?v=5e63aHK6Q-E
+int isCyclicRotation(string &p, string &q) 
+{
+    string text = p + p;
+    string pattern = q;
+    
+    int n = q.size(), m = text.size();
+    int lps[n];
+    
+    lps[0] = 0;
+    
+    int j = 0, i = 1;
+    
+    while(i < n){
+        if(q[i] == q[j]){
+            lps[i] = j + 1;
+            i++;
+            j++;
+        } 
+        else{
+            if(j == 0){
+                lps[i] = 0;
+                i++;
+            }
+            else{
+                j = lps[j-1];
+            }
+        }
+    }
+    
+    int p1 = 0, p2 = 0;
+    
+    while(p1 < m){
+        
+        if(text[p1] == pattern[p2]){
+            p1++;
+            p2++;
+            if(p2 == n){
+              return 1;
+            }
+        }
+        else{
+            if(p2 != 0) p2 = lps[p2 - 1];
+            else		p1++;
+        }
+    }
+    return 0;
+}
