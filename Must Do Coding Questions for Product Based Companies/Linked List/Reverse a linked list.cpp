894e971f2295d409df65f449679a65f772f01313
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-09-27 10:57:23 +0530

    Update Reverse a linked list.cpp

--- Must Do Coding Questions for Product Based Companies/Linked List/Reverse a linked list.cpp
+++ Must Do Coding Questions for Product Based Companies/Linked List/Reverse a linked list.cpp
@@ -1,3 +1,4 @@
+//Iterative
 class Solution
 {
     public:
@@ -20,5 +21,29 @@ class Solution
         temp2 -> next = temp1;
         return temp2;
     }
-    
+};
+
+//Recursive
+//Ref: https://www.youtube.com/watch?v=KYH83T4q6Vs
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    void reverse(Node* head, Node* &root){
+        
+        if(head -> next == NULL){
+            root = head;
+            return;
+        }
+        reverse(head -> next, root);
+        Node* temp = head -> next;
+        temp -> next = head;
+        head -> next = NULL;
+    }
+    struct Node* reverseList(struct Node *head)
+    {
+        Node* root;
+        reverse(head, root);
+        return root;
+    }  
 };
