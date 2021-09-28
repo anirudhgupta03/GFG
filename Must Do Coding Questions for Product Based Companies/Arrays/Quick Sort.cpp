b6547c504281437e592af15bd24867826d103503
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-09-28 18:28:34 +0530

    Create Quick Sort.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Arrays/Quick Sort.cpp
@@ -0,0 +1,31 @@
+class Solution{
+public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        if(low < high){
+            int pivot = partition(arr,low,high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       int pivot = arr[low];
+       
+       int i = low, j = high;
+       
+       while(i < j){
+           
+           while(arr[i] <= pivot) i++;
+           while(arr[j] > pivot) j--;
+           
+           if(i < j) swap(arr[i],arr[j]);
+       }
+       
+       swap(arr[j], arr[low]);
+       return j;
+    }
+};
