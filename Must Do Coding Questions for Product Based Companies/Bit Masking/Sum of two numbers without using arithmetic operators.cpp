fc9d135935d2ca711de675ba4b3b7983e3c049d7
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-22 22:16:59 +0530

    Update Sum of two numbers without using arithmetic operators.cpp

--- Must Do Coding Questions for Product Based Companies/Bit Masking/Sum of two numbers without using arithmetic operators.cpp
+++ Must Do Coding Questions for Product Based Companies/Bit Masking/Sum of two numbers without using arithmetic operators.cpp
@@ -1,3 +1,28 @@
+//Method - 1
+class Solution
+{
+    public:
+    int sum(int a , int b)
+    {
+        int res = 0, carry = 0, i = 0;
+        while(a || b){
+            int bit1 = (a & 1);
+            int bit2 = (b & 1);
+            
+            int bit = bit1 ^ bit2 ^ carry;
+            carry = bit1 & bit2 | bit1 & carry | bit2 & carry;
+           
+            if(bit) res |= (1 << i);
+            a >>= 1;
+            b >>= 1;
+            i++;
+        }
+        if(carry) res |= (1 << i);
+        return res;
+    }
+};
+
+//Method - 2
 class Solution
 {
     public:
