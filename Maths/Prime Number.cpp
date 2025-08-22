52cdab74491f2075e3af951923f7ca5283a36bf4
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-22 23:33:08 +0530

    Create Prime Number.cpp

---
+++ Maths/Prime Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isPrime(int n) {
+        // code here
+        if(n == 1){
+            return false;
+        }
+        for(int i = 2; i <= sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
