1bd0e98da8979c37a3f2fc0d9b84c4688d319ec3
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-26 18:28:56 +0530

    Create Longest Subarray with Sum K.cpp

---
+++ Array/Longest Subarray with Sum K.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+        unordered_map<int,int> umap;
+        int sum = 0;
+        umap[0] = -1;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr[i];
+            if(umap.find(sum) == umap.end()){
+                umap[sum] = i;
+            }
+            
+            if(umap.find(sum - k) != umap.end()){
+                ans = max(ans, i - umap[sum - k]);
+            }
+        }
+        return ans;
+    }
+};
