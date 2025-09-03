b857f9ee4ee4f8a42c2a72457ae3722e45026dff
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-03 14:58:30 +0530

    Update Merge Without Extra Space .cpp

--- Array/Merge Without Extra Space .cpp
+++ Array/Merge Without Extra Space .cpp
@@ -1,7 +1,7 @@
+//Ref: https://www.youtube.com/watch?v=n7uwj04E0I4&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=24
 //Start comparing from the last of the first array and from the start of the second array
 //then check if the element in the array is greater then swap with the element in the second array else break
-
-
+//Method - 1
 class Solution{
 public:
 	void merge(int arr1[], int arr2[], int n, int m) {
@@ -19,4 +19,44 @@ public:
 	        sort(arr1,arr1+n);
 	        sort(arr2,arr2+m);
 	}
+};
+
+//Method - 2
+//Gap Method
+//TC: O((N+M)*log(N+M))
+//SC: O(1)
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size(), m = b.size();
+        int len = n + m;
+        int gap = (len/2) + (len%2);
+        
+        while(gap > 0){
+            int left = 0;
+            int right = gap;
+            while(right < len){
+                if(left < n && right >= n){
+                    if(a[left] > b[right - n]){
+                        swap(a[left], b[right - n]);
+                    }
+                }
+                else if(left >= n){
+                    if(b[left - n] > b[right - n]){
+                        swap(b[left - n], b[right - n]);
+                    }
+                }
+                else{
+                    if(a[left] > a[right]){
+                        swap(a[left], a[right]);
+                    }
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1) break;
+            gap = (gap/2) + (gap %2);
+        }
+    }
 };
