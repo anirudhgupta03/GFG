c0f0045fb1da9a179d6ebee407e7926ee999191e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-09 10:48:44 +0530

    Create Ceil in a Sorted Array.cpp

---
+++ Binary Search/Ceil in a Sorted Array.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+class Solution {
+  public:
+    int findCeil(vector<int>& arr, int x) {
+        // code here
+        int n = arr.size();
+        int ans = -1;
+        
+        int lo = 0, hi = n - 1;
+        
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
