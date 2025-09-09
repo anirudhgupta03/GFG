e5c7c56d254cb85e8c849cd4ccc8a39b5e5982a0
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-09 10:45:25 +0530

    Create Implement Lower Bound.cpp

---
+++ Binary Search/Implement Lower Bound.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        int lo = 0, hi = n - 1, ans = n;
+        
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] >= target){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else if(arr[mid] < target){
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
