d8efb8be0a828aa5cf61bc116ac56825650d152f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-11 10:08:56 +0530

    Create Square Root.cpp

---
+++ Binary Search/Square Root.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // code here
+        int lo = 1, hi = n;
+        int ans;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(mid * mid <= n){
+                ans = mid;
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
