12b6b4bba78febda0b8a5e351ed60028fca83dd4
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-25 18:25:23 +0530

    Create Linked List Insertion At Beginning.cpp

---
+++ Must Do Coding Questions for Product Based Companies/Linked List/Linked List Insertion At Beginning.cpp
@@ -0,0 +1,22 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *insertAtFront(Node *head, int x) {
+        // Code here
+        Node* node = new Node(x);
+        node -> next = head;
+        return node;
+    }
+};
