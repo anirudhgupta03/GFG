599d8ff01d7e40638a76f2c8b6054e2c1d3bed50
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-27 13:38:11 +0530

    Update Sort a linked list of 0s, 1s and 2s.cpp

--- Must Do Coding Questions for Product Based Companies/Linked List/Sort a linked list of 0s, 1s and 2s.cpp
+++ Must Do Coding Questions for Product Based Companies/Linked List/Sort a linked list of 0s, 1s and 2s.cpp
@@ -1,3 +1,71 @@
+//Ref: https://www.youtube.com/watch?v=gRII7LhdJWc&list=PLgUwDviBIf0rAuz8tVcM0AymmhTRsfaLU&index=8
+
+//Brute Force Solution
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        if(head == NULL || head -> next == NULL) return head;
+      
+        Node* dummy1 = new Node(-1);
+        Node* dummy2 = new Node(-1);
+        Node* dummy3 = new Node(-1);
+        
+        Node* ptr1 = dummy1, *ptr2 = dummy2, *ptr3 = dummy3;
+        
+        Node* ptr = head;
+        
+        while(ptr != NULL){
+            Node* nextNode = ptr -> next;
+            if(ptr -> data == 0){
+                ptr1 -> next = ptr;
+                ptr -> next = NULL;
+                ptr1 = ptr;
+            }
+            else if(ptr -> data == 1){
+                ptr2 -> next = ptr;
+                ptr -> next = NULL;
+                ptr2 = ptr;
+            }
+            else{
+                ptr3 -> next = ptr;
+                ptr -> next = NULL;
+                ptr3 = ptr;
+            }
+            ptr = nextNode;
+        }
+        
+        Node* dummyNode = new Node(-1);
+        head = dummyNode;
+        if(dummy1 -> next){
+            dummyNode -> next = dummy1 -> next;
+            dummyNode = ptr1;
+        }
+        if(dummy2 -> next){
+            dummyNode -> next = dummy2 -> next;
+            dummyNode = ptr2;
+        }
+        if(dummy3 -> next){
+            dummyNode -> next = dummy3 -> next;
+            dummyNode = ptr3;
+        }
+        return head -> next;
+    }
+};
+
+//Optimal Solution
 /* Node is defined as
   class Node {
   public:
