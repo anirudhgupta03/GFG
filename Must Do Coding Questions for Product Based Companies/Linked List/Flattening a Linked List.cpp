3d19a2e14bf788ebd7a27cf8e39693396fd2da02
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-10-07 21:01:30 +0530

    Update Flattening a Linked List.cpp

--- Must Do Coding Questions for Product Based Companies/Linked List/Flattening a Linked List.cpp
+++ Must Do Coding Questions for Product Based Companies/Linked List/Flattening a Linked List.cpp
@@ -1,85 +1,48 @@
+//Ref: https://www.youtube.com/watch?v=ykelywHJWLg&list=PLgUwDviBIf0rAuz8tVcM0AymmhTRsfaLU&index=25
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* bottom;
 
-/* Node structure  used in the program
-
-struct Node{
-	int data;
-	struct Node * next;
-	struct Node * bottom;
-	
-	Node(int x){
-	    data = x;
-	    next = NULL;
-	    bottom = NULL;
-	}
-	
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
 };
 */
-
-/*  Function which returns the  root of 
-    the flattened linked list. */
-Node* merge(Node* n1, Node* n2){
-    
-    if(n1 == NULL && n2 == NULL){
-        return NULL;
-    }
-    if(n1 == NULL){
-        return n2;
-    }
-    if(n2 == NULL){
-        return n1;
-    }
-    
-    Node* head, *prev = NULL;
-    
-    while(n1 && n2){
-        
-        if(n1 -> data < n2 -> data){
-            if(prev == NULL){
-                head = n1;
+class Solution {
+  public:
+    Node* merge(Node* n1, Node* n2){
+        Node* dummy = new Node(-1);
+        Node* temp = dummy;
+        while(n1 != NULL && n2 != NULL){
+            if(n1 -> data < n2 -> data){
+                Node* nextNode = n1 -> bottom;
+                temp -> bottom = n1;
+                n1 = nextNode;
+                temp = temp -> bottom;
             }
             else{
-                prev -> bottom = n1;
+                Node* nextNode = n2 -> bottom;
+                temp -> bottom = n2;
+                n2 = nextNode;
+                temp = temp -> bottom;
             }
-            prev = n1;
-            n1 = n1 -> bottom;
         }
-        else{
-            if(prev == NULL){
-                head = n2;
-            }
-            else{
-                prev -> bottom = n2;
-            }
-            prev = n2;
-            n2 = n2 -> bottom;
+        if(n1) temp -> bottom = n1;
+        if(n2) temp -> bottom = n2;
+        return dummy -> bottom;
+    }
+    Node *flatten(Node *root) {
+        // code here
+        if(root == NULL || root -> next == NULL){
+            return root;
         }
+        Node* mergedHead = flatten(root -> next);
+        root = merge(root, mergedHead);
+        return root;
     }
-    if(n1){
-        prev -> bottom = n1;
-    }
-    if(n2){
-        prev -> bottom = n2;
-    }
-    
-    return head;
-}
-Node *flatten(Node *root)
-{
-   if(root == NULL || root -> next == NULL){
-       return root;
-   }
-   
-   Node* temp1 = root, *temp2 = root -> next;
-   
-   temp1 = merge(temp1,temp2);
-  
-   root = root -> next -> next;
-  
-   while(root){
-       temp2 = root;
-       root = root -> next;
-       temp1 = merge(temp1,temp2);
-   }
-   
-   return temp1;
-}
+};
