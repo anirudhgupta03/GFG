a6bffa0f90cb483822659057f88cbccaf67793a9
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-18 09:45:34 +0530

    Update Minimum sum partition.cpp

--- Dynamic Programming/Minimum sum partition.cpp
+++ Dynamic Programming/Minimum sum partition.cpp
@@ -62,50 +62,35 @@ class Solution{
 //Bottom-Up Approach
 //Time Complexity - O(N*sum)
 //Space Complexity - O(N*sum)
-class Solution{
-
-	public:
-	int minDiffernce(int arr[], int n) 
-	{ 
-	    int sum = 0;
-	    
-	    for(int i = 0; i < n; i++){
-	        sum += arr[i];
-	    }
-	    
-	    int dp[n+1][sum+1];
-	    for(int i = 0; i < n + 1; i++){
-	        dp[i][0] = 1;
-	    }
-	    for(int j = 1; j < sum + 1; j++){
-	        dp[0][j] = 0;
-	    }
-	    
-	    for(int i = 1; i < n + 1; i++){
-	        for(int j = 1; j < sum + 1; j++){
-	            
-	            if(arr[i-1] <= j){
-	                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j]; 
-	            }
-	            else{
-	                dp[i][j] = dp[i-1][j];
-	            }
-	        }
-	    }
-	    
-	    vector<int> v;
-	    
-	    for(int j = 0; j <= sum/2; j++){
-	        if(dp[n][j]){
-	            v.push_back(j);
-	        }
-	    }
-	    
-	    int mindiff = INT_MAX;
-	    for(int i = 0; i < v.size(); i++){
-	        mindiff = min(mindiff,sum - 2*v[i]);
-	    }
-	    
-	    return mindiff;
-	} 
+class Solution {
+  public:
+    int minDifference(vector<int>& arr) {
+        // Your code goes here
+        int n = arr.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= sum; j++){
+                if(j == 0) dp[i][j] = 1;
+                else if(i == 0) dp[i][j] = 0;
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                    if(arr[i - 1] <= j){
+                        dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
+                    }
+                }
+            }
+		}
+        int minDiff = INT_MAX;
+        for(int j = 0; j <= sum/2; j++){
+            int s1 = j, s2 = sum - j;
+            if(dp[n][s1]){
+                minDiff = min(minDiff, abs(s1 - s2));
+            }
+        }
+        return minDiff;
+    }
 };
