ffb442ad07a88165bdfcc28342126531926b5777
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-18 10:33:09 +0530

    Update Partitions with Given Difference.cpp

--- Dynamic Programming/Partitions with Given Difference.cpp
+++ Dynamic Programming/Partitions with Given Difference.cpp
@@ -1,3 +1,4 @@
+//Tabulation 
 class Solution {
   public:
     int countPartitions(vector<int>& arr, int diff) {
@@ -43,5 +44,56 @@ class Solution {
             }
         }
         return dp[n][psum]*pow(2, zeros);
+    }
+};
+
+//Tabulation + Space Optimization
+class Solution {
+  public:
+    int countPartitions(vector<int>& arr, int diff) {
+        // Code here
+        int sum = 0;
+        
+        vector<int> vec;
+        int zeros = 0;
+        
+        for(int &ele: arr){
+            if(ele == 0){
+                zeros++;
+            }
+            else{
+                vec.push_back(ele);
+            }
+            sum += ele;
+        }
+        
+        if((sum + diff)%2 != 0) return 0;
+        
+        int psum = (sum + diff)/2;
+        
+        int n = vec.size();
+        vector<int> dp(psum + 1, 0);
+        
+        for(int i = 0; i <= n; i++){
+            vector<int> currdp(psum + 1, 0);
+            for(int j = 0; j <= psum; j++){
+                if(j == 0){
+                    currdp[j] = 1;
+                }
+                else if(i == 0){
+                    currdp[j] = 0;
+                }
+                else{
+                    if(vec[i - 1] <= j){
+                        currdp[j] = dp[j - vec[i - 1]] + dp[j];
+                    }
+                    else{
+                        currdp[j] = dp[j];
+                    }
+                }
+            }
+            dp = currdp;
+        }
+        return dp[psum]*pow(2, zeros);
     }
 };
