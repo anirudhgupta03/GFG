917be3cf333736191a5bc3380a0070a42991af2d
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2026-01-02 11:41:16 +0530

    Update Top View of Binary Tree.cpp

--- Trees/Top View of Binary Tree.cpp
+++ Trees/Top View of Binary Tree.cpp
@@ -5,44 +5,6 @@ class Solution
     //from left to right in Binary Tree.
     vector<int> topView(Node *root)
     {
-        map<int,vector<int>> mp;
-        
-        vector<int> res;
-        
-        if(root == NULL){
-            return res;
-        }
-        queue<pair<Node*,int>> q;
-        q.push({root,0});
-        
-        while(!q.empty()){
-            
-            Node* curr = q.front().first;
-            int d = q.front().second;
-            q.pop();
-            
-            mp[d].push_back(curr -> data);
-            if(curr -> left) q.push({curr -> left,d-1});
-            if(curr -> right) q.push({curr -> right, d+1});
-        }
-        vector<int> temp;
-        for(auto x: mp){
-            temp = x.second;
-            res.push_back(temp[0]);
-        }
-        return res;
-    }
-
-};
-
-//Alter
-class Solution
-{
-    public:
-    //Function to return a list of nodes visible from the top view 
-    //from left to right in Binary Tree.
-    vector<int> topView(Node *root)
-    {
         vector<int> res;
         
         if(root == NULL){
@@ -72,5 +34,4 @@ class Solution
         }
         return res;
     }
-
 };
