0925ab585bb45d5b227ed447caf6d98b8a54dbc1
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2026-01-02 10:29:56 +0530

    Create Tree Boundary Traversal.cpp

---
+++ Trees/Tree Boundary Traversal.cpp
@@ -0,0 +1,63 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> boundaryTraversal(Node *root) {
+        // code here
+        vector<int> res;
+        if(root == NULL) return res;
+        if(root -> left == NULL && root -> right == NULL) return {root -> data};
+        
+        res.push_back(root -> data);
+        
+        Node* temp = root -> left;
+        while(temp){
+            if(temp -> left || temp -> right){
+                res.push_back(temp -> data);
+            }
+            if(temp -> left == NULL) temp = temp -> right;
+            else temp = temp -> left;
+        }
+        
+        stack<Node*> st;
+        st.push(root);
+        while(!st.empty()){
+            Node* curr = st.top();
+            st.pop();
+            if(curr -> left == NULL && curr -> right == NULL){
+                res.push_back(curr -> data);
+            }
+            if(curr -> right) st.push(curr -> right);
+            if(curr -> left) st.push(curr -> left);
+        }
+        
+        temp = root -> right;
+        while(temp){
+            if(temp -> left || temp -> right){
+                st.push(temp);
+            }
+            if(temp -> right == NULL) temp = temp -> left;
+            else temp = temp -> right;
+        }
+        
+        while(!st.empty()){
+            res.push_back(st.top() -> data);
+            st.pop();
+        }
+        return res;
+    }
+};
